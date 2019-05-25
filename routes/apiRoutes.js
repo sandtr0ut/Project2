3eaf75a6f02ffe22dfc3860e062a94aa3fd09197
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-05-25 10:27:21 -0700

    Added user route boilerplate code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,23 @@
 // var db = require("../models");
 
+// TODO: Implement authentication
+
 module.exports = function(app) {
     app.get("/api/user/:id", function(request, response) {
-        response.send("Hello, world!");
+        // TODO: Implement retrieving a user based on the given id from db
+        // const { id } = request.params;
+        response.send("Hello from GET!");
+    });
+    app.post("/api/user", function(request, response) {
+        // TODO: Implement creation of new users
+        response.send("Hello from POST!");
+    });
+    app.put("/api/user", function(request, response) {
+        // TODO: Implement updating of users data
+        response.send("Hello from PUT!");
+    });
+    app.delete("/api/user", function(request, response) {
+        // TODO: Implement user deletion
+        response.send("Hello from DELETE!");
     });
 };
