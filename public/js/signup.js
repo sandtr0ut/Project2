8b3fcd7a0e6883df82d9603cd10e8ada011042b0
sandtr0ut <crosbyverse@gmail.com>
2019-05-29 01:40:25 -0700

    code temp, non-exphbs html; user routes; and html routes

---
+++ public/js/signup.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    
+    //globals
+    var signupForm = $("#signup");
+    var firstNameInput = $("#inputFirst").val().trim();
+    var lastNameInput = $("#inputLast").val().trim();
+    var emailInput = $("#inputEmail").val().trim();
+    var passwordInput = $("#inputPW").val().trim();
+    var nfl = $("#nflCheck").prop("checked");
+    var nba = $("#nbaCheck").prop("checked");
+    var mlb = $("#mlbCheck").prop("checked");
+    var nhl = $("#nhlCheck").prop("checked");
+    var ncaaf = $("#ncaafCheck").prop("checked");
+    var ncaab = $("#ncaabCheck").prop("checked");
+    
+    //event-listener for form-submission
+    $(document).on("submit", "#signup", handleSignupFormSubmit);
+    $(document).on("click", "#cancel", handleCancel);
+    
+
+    function handleSignupFormSubmit(event) {
+        event.preventDefault();
+        if (!firstNameInput || !lastNameInput || !emailInput || !passwordInput) {
+            return;
+        }
+        
+        createUser({
+            first: firstNameInput,
+            last: lastNameInput,
+            email: emailInput,
+            password: passwordInput,
+            nfl: nfl,
+            nba: nba,
+            mlb: mlb,
+            nhl: nhl,
+            ncaaf: ncaaf,
+            ncaab: ncaab
+        })
+    }
+    
+    function createUser(userData) {
+        $.post("/api/users", userData)
+        .then(loadDashboard);
+    }
+    
+    function loadDashboard() {
+        window.location.href = "/dashboard.html"
+    }
+    
+    function handleCancel() {
+        window.location.href = "/home.html"
+    }
+    
+});
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,50 @@
 // var db = require("../models");
 
+var path = require("path");
+
 module.exports = function(app) {
     // Load index page
     app.get("/", function(request, response) {
-        response.render("index", request);
+        response.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+      
+    app.get("/dashboard", function(request, response) {
+        response.sendFile(path.join(__dirname, "../public/dashboard.html"));
     });
 
     // Load example page and pass in an example by id
     app.get("/login", function(request, response) {
-        response.render("login", request);
+        response.sendFile(path.join(__dirname, "../public/login.html"));
     });
 
     app.get("/signup", function(request, response) {
-        response.render("signup", request);
+        response.sendFile(path.join(__dirname, "../public/signup.html"));
     });
 
     app.get("/preferences", function(request, response) {
-        response.render("preferences", request);
+        response.sendFile(path.join(__dirname, "../public/preferences.html"));
     });
+
+    // app.get("/", function(request, response) {
+    //     response.render("index", request);
+    // });
+    
+    // app.get("/dashboard", function(request, response) {
+    //     response.render("dashboard", request);
+    // });
+
+    // // Load example page and pass in an example by id
+    // app.get("/login", function(request, response) {
+    //     response.render("login", request);
+    // });
+
+    // app.get("/signup", function(request, response) {
+    //     response.render("signup", request);
+    // });
+
+    // app.get("/preferences", function(request, response) {
+    //     response.render("preferences", request);
+    // });
 
     // Render 404 page for any unmatched routes
     app.get("*", function(request, response) {
