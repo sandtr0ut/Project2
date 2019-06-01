3d810b6a8e32673e88608b7efd3d9c97782b8c37
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-06-01 11:47:17 -0700

    Refractored out user routes to seperate file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,9 @@
-// var db = require("../models");
 const apis = require("../controllers/externalAPI");
 
 module.exports = function(app) {
-    app.get("/api/user", function(request, response) {
-        // const { id } = request.body;
-        // App will take an id from the request body, get the preferences from the database (via Sequelize),
-        // then make a request to the external API, returning both the pulled data combined with the user data
-        let dummyData = {
-            user: {
-                username: "bob123",
-                password: "password",
-                preferences: ["americanfootball_nfl"],
-            },
-        };
+    // App will take an id from the request body, get the preferences from the database (via Sequelize),
+    // then make a request to the external API, returning both the pulled data combined with the user data
+    app.get("/api/theodds/odds", function(request, response) {
         apis.theOdds.get
             .odds("americanfootball_nfl", "us")
             .then(requestedData => {
@@ -21,9 +12,9 @@ module.exports = function(app) {
             })
             .catch(err => console.log(err));
     });
-    app.get("/api/sports", function(request, response) {
-        // Route will serve a list of available sports and their corresponding id's, mainly to provide a choice of sports to follow.
-        // Sports data changes by season so list is updated dynamically through external api
+
+    // TODO: Add documentation
+    app.get("/api/theodds/sports", function(request, response) {
         apis.theOdds.get
             .sports()
             .then(requestedData => {
@@ -32,10 +23,22 @@ module.exports = function(app) {
             })
             .catch(err => console.log(err));
     });
-    // Active Sports from TheRundown
-    app.get("/api/rdsports", function(request, response) {
-        // Route will serve a list of available sports and their corresponding id's, mainly to provide a choice of sports to follow.
-        // Sports data changes by season so list is updated dynamically through external api
+
+    // Route will serve a list of available sports and their corresponding id's, mainly to provide a choice of sports to follow.
+    // Sports data changes by season so list is updated dynamically through external api
+    app.get("/api/sports", function(request, response) {
+        apis.theOdds.get
+            .sports()
+            .then(requestedData => {
+                const { data } = requestedData;
+                response.json(data);
+            })
+            .catch(err => console.log(err));
+    });
+
+    // Route will serve a list of available sports and their corresponding id's, mainly to provide a choice of sports to follow.
+    // Sports data changes by season so list is updated dynamically through external api
+    app.get("/api/rundown/sports", function(request, response) {
         apis.theRundown.get
             .sports()
             .then(requestedData => {
@@ -44,11 +47,10 @@ module.exports = function(app) {
             })
             .catch(err => console.log(err));
     });
-    
-    //Events by sport id
-    app.get("/api/events", function(request, response) {
-        // Route will serve a list of available sports and their corresponding id's, mainly to provide a choice of sports to follow.
-        // Sports data changes by season so list is updated dynamically through external api
+
+    // Route will serve a list of available sports and their corresponding id's, mainly to provide a choice of sports to follow.
+    // Sports data changes by season so list is updated dynamically through external api
+    app.get("/api/rundown/events", function(request, response) {
         apis.theRundown.get
             .events()
             .then(requestedData => {
@@ -56,22 +58,5 @@ module.exports = function(app) {
                 response.json(data);
             })
             .catch(err => console.log(err));
-    });
-    
-    app.post("/api/user", function(request, response) {
-        // TODO: Implement creation of new users
-        response.send("Hello from POST!");
-    });
-    app.put("/api/user", function(request, response) {
-        // TODO: Implement updating of users data
-        response.send("Hello from PUT!");
-    });
-    app.delete("/api/user", function(request, response) {
-        // TODO: Implement user deletion
-        response.send("Hello from DELETE!");
-    });
-    app.get("/api/login", function(request, response) {
-        // TODO: Implement login authorization
-        response.send("Hello from login!");
     });
 };
