96ac07ff61159b61626f96e6e4f9bf9e634c84be
Brandon Burrus <brandonburrus.biz@gmail.com>
2019-06-01 13:34:30 -0700

    Updated external api usage to use request query

--- controllers/externalAPI.js
+++ controllers/externalAPI.js
@@ -2,49 +2,48 @@
 require("dotenv").config();
 const axios = require("axios");
 
+// TODO: Implement cycling of API key usage.
+
 module.exports = {
     theOdds: {
         get: {
-            sports: function() {
+            sports: function(query = {}) {
                 return axios.get("https://api.the-odds-api.com/v3/sports", {
-                    params: {
+                    params: Object.assign(query, {
                         api_key: "174f173495f284be13b11968a2dcca1e",
                         // api_key: process.env.THE_ODDS_API_KEY,
-                        all: 1,
-                    },
+                    }),
                 });
             },
-            odds: function(sport, region) {
+            odds: function(query = {}) {
                 return axios.get("https://api.the-odds-api.com/v3/odds", {
-                    params: {
+                    params: Object.assign(query, {
                         api_key: "174f173495f284be13b11968a2dcca1e",
                         // api_key: process.env.THE_ODDS_API_KEY,
-                        sport: sport,
-                        region: region,
-                    },
+                    }),
                 });
             },
         },
     },
-    
     theRundown: {
         get: {
             sports: function() {
                 return axios.request({
                     url: "https://therundown-therundown-v1.p.rapidapi.com/sports",
                     method: "get",
-                    headers: {"X-RapidAPI-Host": "therundown-therundown-v1.p.rapidapi.com"},
-                    headers: {"X-RapidAPI-Key": "ce809ebaf1mshffed23de73fa792p1c723djsn42af7df6070e"},
+                    headers: { "X-RapidAPI-Host": "therundown-therundown-v1.p.rapidapi.com" },
+                    headers: { "X-RapidAPI-Key": "ce809ebaf1mshffed23de73fa792p1c723djsn42af7df6070e" },
                 });
             },
             events: function() {
                 return axios.request({
-                    url: "https://therundown-therundown-v1.p.rapidapi.com/sports/3/events?include=all_periods%2C+scores%2C+and%2For+teams",
+                    url:
+                        "https://therundown-therundown-v1.p.rapidapi.com/sports/3/events?include=all_periods%2C+scores%2C+and%2For+teams",
                     method: "get",
-                    headers: {"X-RapidAPI-Host": "therundown-therundown-v1.p.rapidapi.com"},
-                    headers: {"X-RapidAPI-Key": "ce809ebaf1mshffed23de73fa792p1c723djsn42af7df6070e"},
+                    headers: { "X-RapidAPI-Host": "therundown-therundown-v1.p.rapidapi.com" },
+                    headers: { "X-RapidAPI-Key": "ce809ebaf1mshffed23de73fa792p1c723djsn42af7df6070e" },
                 });
             },
-        }
-    }
+        },
+    },
 };
