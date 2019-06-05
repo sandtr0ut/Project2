99ad0a9f52f6d6536098a34930a06f41e232bcd0
sandtr0ut <crosbyverse@gmail.com>
2019-06-05 14:42:11 -0700

    update sequelize definitions - add basic validation

--- models/user.js
+++ models/user.js
@@ -1,11 +1,34 @@
 /* eslint-disable prettier/prettier */
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        first: DataTypes.STRING,
-        last: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        preferences: DataTypes.JSON
+        first: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        last: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
     });
     return User;
 };
--- public/js/bulma.js
+++ public/js/bulma.js
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 (function() {
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+    var burger = document.querySelector(".burger");
+    var menu = document.querySelector("#"+burger.dataset.target);
+    burger.addEventListener("click", function() {
+        burger.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
     });
 })();
