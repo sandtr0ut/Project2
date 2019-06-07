cc51ed58c8c73b27e71d23ede9386202302f2885
sandtr0ut <crosbyverse@gmail.com>
2019-06-07 13:35:19 -0700

    create tracked branch

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,34 @@
 /* eslint-disable prettier/prettier */
 // var db = require("../models");
 
+const apis = require("../controllers/externalAPI");
+
 module.exports = function(app) {
     // Load index page
     app.get("/", function(request, response) {
         response.render("index", request);
     });
-
     // Load example page and pass in an example by id
     app.get("/login", function(request, response) {
         response.render("login", request);
     });
-
     app.get("/signup", function(request, response) {
         response.render("signup", request);
     });
-
     app.get("/preferences", function(request, response) {
         response.render("preferences", request);
     });
-
+    app.get("/mlb", function(request, response) {
+        
+        response.render("mlb", request);
+        res.render("mlb", {
+            sport: "Major League Baseball",
+            events: dbExamples
+    });
+    
+    
+    
+    
     // Render 404 page for any unmatched routes
     app.get("*", function(request, response) {
         response.render("404", response);
