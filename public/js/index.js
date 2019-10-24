386c9ac55291472a1822a4ae242281e0efb0d5ba
sandtr0ut <crosbyverse@gmail.com>
2019-10-24 12:58:32 -0700

    update db ref to User from Example

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-
 var $firstName = $("#first");
 var $lastName = $("#last");
 var $email = $("#email");
@@ -7,114 +5,111 @@ var $password = $("#password");
 var $preferences = {};
 var $submitBtn = $("#submit");
 
-
 var API = {
-    // Create
-    saveUser: function(user) {
-        return $.ajax({
-            headers: {
-                "Content-Type": "application/json"
-            },
-            type: "POST",
-            url: "/user",
-            data: JSON.stringify(user)
-        });
-    },
-    // Read
-    getUserInfo: function(id) {
-        return $.ajax({
-            url: "/user/:" + id,
-            type: "GET"
-        });
-    },
-    
-    // Update
-    updateInfo: function(id) {
-        return $.ajax({
-            url: "/user/:" + id,
-            type: "GET"
-        });
-    },
-    
-    //Delete
-    deleteUser: function(id) {
-        return $.ajax({
-            url: "/user/:" + id,
-            type: "DELETE"
-        });
-    }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-    API.getExamples().then(function(data) {
-        var $examples = data.map(function(example) {
-            var $a = $("<a>")
-                .text(example.text)
-                .attr("href", "/example/" + example.id);
-
-            var $li = $("<li>")
-                .attr({
-                    class: "list-group-item",
-                    "data-id": sport.id
-                })
-                .append($a);
-
-            var $button = $("<button>")
-                .addClass("btn btn-danger float-right delete")
-                .text("ｘ");
-
-            $li.append($button);
-
-            return $li;
-        });
-
-        $exampleList.empty();
-        $exampleList.append($examples);
+  // Create
+  saveUser: function(user) {
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "/user",
+      data: JSON.stringify(user)
     });
+  },
+  // Read
+  getUserInfo: function(id) {
+    return $.ajax({
+      url: "/user/:" + id,
+      type: "GET"
+    });
+  },
+
+  // Update
+  updateInfo: function(id) {
+    return $.ajax({
+      url: "/user/:" + id,
+      type: "GET"
+    });
+  },
+
+  //Delete
+  deleteUser: function(id) {
+    return $.ajax({
+      url: "/user/:" + id,
+      type: "DELETE"
+    });
+  }
 };
 
+// refreshUsers gets new users from the db and repopulates the list
+var refreshUsers = function() {
+  API.getUsers().then(function(data) {
+    var $users = data.map(function(user) {
+      var $a = $("<a>")
+        .text(user.text)
+        .attr("href", "/user/" + user.id);
+
+      var $li = $("<li>")
+        .attr({
+          class: "list-group-item",
+          "data-id": sport.id
+        })
+        .append($a);
+
+      var $button = $("<button>")
+        .addClass("btn btn-danger float-right delete")
+        .text("ｘ");
+
+      $li.append($button);
+
+      return $li;
+    });
+
+    $userList.empty();
+    $userList.append($users);
+  });
+};
 
 // Save the new user to the db
 var handleFormSubmit = function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var newUser = {
-        firstName: $firstName.val().trim(),
-        lastName: $lastName.val().trim(),
-        email: $email.val().trim(),
-        password: $password.val().trim(),
-        preferences: $preferences
-    };
+  var newUser = {
+    firstName: $firstName.val().trim(),
+    lastName: $lastName.val().trim(),
+    email: $email.val().trim(),
+    password: $password.val().trim(),
+    preferences: $preferences
+  };
 
-    if (!(newUser.email && newUser.password)) {
-        alert("You must enter your email and password!");
-        return;
-    }
+  if (!(newUser.email && newUser.password)) {
+    alert("You must enter your email and password!");
+    return;
+  }
 
-    API.saveUser(newUser).then(function() {
-        alert("User Added");
-        
-    });
+  API.saveUser(newUser).then(function() {
+    alert("User Added");
+  });
 
-    $firstName.val("");
-    $lastName.val("");
-    $email.val("");
-    $password.val("");
+  $firstName.val("");
+  $lastName.val("");
+  $email.val("");
+  $password.val("");
 };
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
+// handleDeleteBtnClick is called when user's delete button is clicked
+// Remove the user from the db and refresh the list
 var handleDeleteBtnClick = function() {
-    var idToDelete = $(this)
-        .parent()
-        .attr("data-id");
+  var idToDelete = $(this)
+    .parent()
+    .attr("data-id");
 
-    API.deleteExample(idToDelete).then(function() {
-        refreshExamples();
-    });
+  API.deleteUser(idToDelete).then(function() {
+    refreshUsers();
+  });
 };
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$userList.on("click", ".delete", handleDeleteBtnClick);
